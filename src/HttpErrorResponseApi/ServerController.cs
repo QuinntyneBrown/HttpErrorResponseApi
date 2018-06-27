8d4e78763c39929a8d27e63e77ac36a32c03693d
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-06-27 14:31:28 -0400

    initial code

---
+++ src/HttpErrorResponseApi/ServerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpErrorResponseApi.Controllers
+{
+    [Route("api/server")]
+    [ApiController]
+    public class ServerController: ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+            => StatusCode(StatusCodes.Status502BadGateway);        
+    }
+}
